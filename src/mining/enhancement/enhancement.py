23d9706ec7756cf25d28af55398dd6c64e615629
Florian Markusse <florianmarkusse@hotmail.com>
2021-04-26 16:47:48 +0200

    julia done?

--- src/mining/enhancement/enhancement.py
+++ src/mining/enhancement/enhancement.py
@@ -8,9 +8,22 @@ def add_human_comments_member(prs):
 
 
 def add_benchmark_bot_free_participants_member(owner, repo, prs):
+    botUserName = subroutines.get_bot_username(owner, repo)
     for pr in prs:
-        botUserName = subroutines.get_bot_username(owner, repo)
         if botUserName in pr["participants"]:
             pr["benchmarkBotFreeParticipants"] = len(pr["participants"]) - 1
         else:
             pr["benchmarkBotFreeParticipants"] = len(pr["participants"])
+
+
+def add_comments_after_benchmarking_bot_contribution(owner, repo, prs):
+    botUserName = subroutines.get_bot_username(owner, repo)
+    for pr in prs:
+        after_benchmark_contribution_count = 0
+        benchmark_bot_contributed = False
+        for comment in pr["commenterAndLengths"]:
+            if benchmark_bot_contributed:
+                after_benchmark_contribution_count += 1
+            if botUserName in comment[0]:
+                benchmark_bot_contributed = True
+        pr["commentsAfterContribution"] = after_benchmark_contribution_count
--- src/pytes.py
+++ src/pytes.py
@@ -1,5 +1,6 @@
 from src.utility import file_management
 from src.mining import collector, matcher
+from src.mining.enhancement import enhancement
 from src.mining.rest import changed_files, participants_bot_callers_comment_lengths, reviewers
 from src.analysis.plotting import qq_plot
 import os
@@ -19,3 +20,10 @@ for project in projects:
 
     data = file_management.get_all_mined_prs(owner, repo)
 
+    enhancement.add_comments_after_benchmarking_bot_contribution(owner, repo, data["bot_prs"])
+    enhancement.add_comments_after_benchmarking_bot_contribution(owner, repo, data["non_bot_prs"])
+
+    file_management.write_data(data["bot_prs"], owner, repo, "botPRs")
+    file_management.write_data(data["non_bot_prs"], owner, repo, "nonBotPRs")
+
+    matcher.do_matchings(owner, repo, data.get("bot_prs"), data.get("non_bot_prs"))
