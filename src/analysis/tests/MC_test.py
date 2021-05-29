804a70723a433a03b1b5931c6d039ae40e3c2a6d
Florian Markusse <florianmarkusse@hotmail.com>
2021-05-29 17:46:22 +0200

    added file writing to stats and CL effect size

--- src/analysis/tests/MC_test.py
+++ src/analysis/tests/MC_test.py
@@ -1,7 +1,18 @@
 import scipy.stats
+from src.utility import file_management
 
 
 def perform_mc_test(owner, repo, name, metric, x, y):
     result = scipy.stats.mannwhitneyu(x, y, True, "two-sided")
     print(f"{owner}/{repo}, dataset: {name} for metric: {metric}")
     print(f"Statistic: {result[0]}, p-value: {result[1]}")
+    common_language_effect_size = result[0] / (len(x) * len(y))
+    print(f"effect size is: {common_language_effect_size}")
+
+    test_result = {
+        "U-statistic": result[0],
+        "p-value": result[1],
+        "CL effect size": common_language_effect_size
+    }
+
+    file_management.write_data(test_result, owner, repo, f"{name}_{metric}")
